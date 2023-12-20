20aa9316c45b18e5ab160b6c70b71f6990cb0d09
clemgrom <clement.gromangin5@etu.univ-lorraine.fr>
2023-12-20 11:25:07 +0100

    pleins de changement sur les jeux création d'un second niveau en sidescrolling

--- src/gaming/level2.js
+++ src/gaming/level2.js
@@ -13,14 +13,17 @@ const config = {
         create: create,
         update: update
     }
-
 }
 
 var game = new Phaser.Game(config);
 let walle;
+let garfi;
 let cursors;
 let qzsd;
 let hitSound;
+let asteroid;
+let background;
+let obstacle2;
 
 function preload() {
     this.load.image('wall-e', 'images/wall-e.png');
@@ -32,13 +35,11 @@ function preload() {
 }
 
 function create() {
-
     hitSound = this.sound.add('hit');
+    background = this.add.tileSprite(0, 0, config.width, config.height, 'background').setOrigin(0, 0);
 
-    this.add.image(0, 0, 'background').setOrigin(0, 0);
     walle = this.physics.add.image(150, 50, 'wall-e');
     garfi = this.physics.add.image(300, 200, 'obstacle');
-    obstacle2 = this.physics.add.staticImage(800, 500, 'obstacle2');
     walle.body.collideWorldBounds = true;
     garfi.body.collideWorldBounds = true;
 
@@ -51,11 +52,24 @@ function create() {
     qzsd = this.input.keyboard.addKeys('Q,Z,S,D');
 
     this.physics.add.collider(walle, garfi);
-    this.physics.add.collider(walle, obstacle2);
-    this.physics.add.collider(garfi, obstacle2);
 
+    obstacles = this.physics.add.group({
+        key: 'obstacle2',
+        repeat: 4,
+        setXY: { x: 0, y: 0, stepX: 200, stepY: 150 }
+    });
+
+    this.physics.add.collider(walle, obstacles, function () {
+        walle.setTint(0xff0000);
+        setTimeout(function () {
+            walle.clearTint();
+        }, 200);
+        
+    });
+
+    this.physics.add.collider(garfi, obstacles);
     this.physics.add.collider(garfi, asteroid);
-    this.physics.add.collider(obstacle2, asteroid);
+    this.physics.add.collider(obstacles, asteroid);
 
     this.physics.add.collider(walle, garfi, function () {
         hitSound.play();
@@ -66,32 +80,41 @@ function create() {
         setTimeout(function () {
             walle.clearTint();
         }, 200);
-        
+    });
+
+    obstacles.children.iterate(function (obstacle) {
+        obstacle.body.setImmovable(true);
     });
 }
 
 function update() {
-    if (cursors.left.isDown) {
-        walle.setVelocityX(-700);
-    } else if (cursors.right.isDown) {
-        walle.setVelocityX(700);
-    } else if (cursors.up.isDown) {
-        walle.setVelocityY(-360);
-    } else if (cursors.down.isDown) {
-        walle.setVelocityY(360);
-    }
+    // Gestion des mouvements de walle
+    walle.setVelocity(cursors.left.isDown ? -700 : cursors.right.isDown ? 700 : 0, cursors.up.isDown ? -360 : cursors.down.isDown ? 360 : 0);
 
-    if (qzsd.Q.isDown) {
-        garfi.setVelocityX(-700);
-    } else if (qzsd.D.isDown) {
-        garfi.setVelocityX(700);
-    } else if (qzsd.Z.isDown) {
-        garfi.setVelocityY(-360);
-    } else if (qzsd.S.isDown) {
-        garfi.setVelocityY(360);
-    }
+    // Gestion des mouvements de garfi
+    garfi.setVelocity(qzsd.Q.isDown ? -700 : qzsd.D.isDown ? 700 : 0, qzsd.Z.isDown ? -360 : qzsd.S.isDown ? 360 : 0);
 
+    // Gestion des collisions avec l'asteroïde
     if (Phaser.Geom.Intersects.RectangleToRectangle(walle.getBounds(), asteroid.getBounds())) {
         asteroid.setVelocity(Phaser.Math.Between(-100, 100), Phaser.Math.Between(-100, 100));
     }
+
+    // Déplacement du fond
+    background.tilePositionX += 1;
+
+    // Mise à jour de la position de tous les obstacles
+    obstacles.children.iterate(function (obstacle) {
+        obstacle.x -= 2;
+        if (obstacle.x < 0) {
+            obstacle.x = config.width;
+        }else if (obstacle.x > config.width) {
+            obstacle.x = 0;
+        }
+        else if (obstacle.y < 0) {
+            obstacle.y = config.height;
+        }
+        else if (obstacle.y > config.height) {
+            obstacle.y = 0;
+        }
+    });
 }
