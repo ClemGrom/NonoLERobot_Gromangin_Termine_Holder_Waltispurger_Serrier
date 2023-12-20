23111a65c7b08f86ab392bed8801348ae6577e14
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2023-12-20 18:35:53 +0100

    update home page et App.jsx

--- front end/src/App.jsx
+++ front end/src/App.jsx
@@ -1,58 +1,59 @@
 import logo from './style/images/logo.svg';
-import './style/sass/_global.scss'
-import React from "react";
+import './style/sass/main.scss'
+import React, {useState} from "react";
 import {
   BrowserRouter as Router,
   Route,
   Link,
-  Routes
+  Routes,
 } from "react-router-dom";
 import Home from "./pages/HomePage/Home";
 import About from "./pages/AboutPage/About";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Router>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link className="App-link" to="/">Home</Link>
-                </li>
-                <li>
-                  <Link className="App-link" to="/about">About</Link>
-                </li>
-                <li>
-                  <Link className="App-link" to="/users">Users</Link>
-                </li>
-              </ul>
-            </nav>
+    const [isMenuOpen, setMenuOpen] = useState(false);
 
-            {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-            </Routes>
-          </div>
-        </Router>
-      </header>
-    </div>
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    };
+
+  return (
+      <div className="App">
+          <Router>
+              <nav id="navigation">
+                  <p>Menu</p>
+                  <div id="menu-icon" onClick={toggleMenu}>
+                      &#9776;
+                  </div>
+                  <ul
+                      className={`menu-transition ${
+                          isMenuOpen
+                              ? 'show-menu menu-transition-enter-active'
+                              : 'menu-transition-leave-active'
+                      }`}
+                  >
+                      {isMenuOpen && (
+                          <>
+                              <li className="menu-transition-enter">
+                                  <Link className="App-link" to="/">Home</Link>
+                              </li>
+                              <li className="menu-transition-enter">
+                                  <Link className="App-link" to="/about">About</Link>
+                              </li>
+                          </>
+                      )}
+                  </ul>
+              </nav>
+
+              <Routes>
+                {/* Le contenu spécifique à la route Home */}
+                <Route path="/" element={<Home />} />
+
+                {/* Le contenu spécifique à la route About */}
+                <Route path="/about" element={<About />} />
+              </Routes>
+          </Router>
+      </div>
   );
 }
-
 export default App;
