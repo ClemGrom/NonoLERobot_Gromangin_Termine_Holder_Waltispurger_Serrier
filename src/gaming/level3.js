1fade64434bd5c1c232b446b98987c62ce121f5c
ClemGrom <clement.gromangin5@etu.univ-lorraine.fr>
2024-02-13 11:30:41 +0100

    plein de trucs

--- src/gaming/level3.js
+++ src/gaming/level3.js
@@ -15,69 +15,81 @@ const config = {
   },
 };
 
-var game = new Phaser.Game(config);
+let game = new Phaser.Game(config);
 let robot;
 let cursors;
 let background;
-let baterie;
+let batterie;
 let caisses;
 let garfi;
-let garfiSpeed = 100;
+let garfiShpeed = 100;
 let gh;
+let sensors = [];
+let sensorGraphics = [];
+let angle;
+let startAngle = -30; // Angle de départ
+let angleIncrement = 30; // Incrément d'angle pour chaque capteur
 
 function preload() {
   this.load.image("robot", "images/wall-e.png");
   this.load.image("background", "images/galaxie.jpg");
-  this.load.image("asteroid", "images/asteroid.png");
-  this.load.image("baterie", "images/petitgarfield.png");
+  this.load.image("asteroide", "images/asteroid.png");
+  this.load.image("batterie", "images/petitgarfield.png");
   this.load.image("caisse", "images/chad.jpg");
   this.load.image("garfi", "images/fusee.png");
 }
 
 function create() {
-  // Crée le background
+  // Background
   background = this.add.image(0, 0, "background");
-  background.setOrigin(0, 0); // Définit l'origine du background à son coin supérieur gauche
+  background.setOrigin(0, 0);
 
   // Définit la taille du monde à la taille du background
   this.physics.world.setBounds(0, 0, background.width, background.height);
 
-  // Crée le robot
+  // Création des entités
+
+  // Création du robot
   robot = this.physics.add.image(750, 300, "robot");
 
-  // Empêche le robot de sortir des limites du monde
-  robot.setCollideWorldBounds(true);
-
-
-  // Fait en sorte que la caméra suive le robot
-  this.cameras.main.startFollow(robot);
-
-  // Fait en sorte que la caméra ne dépasse pas les limites du monde
-  this.cameras.main.setBounds(0, 0, background.width, background.height);
-
-  // Crée l'objet baterie
-  baterie = this.physics.add.image(400, 300, "baterie");
-
-  // Vérifie les collisions entre le robot et la baterie
-  this.physics.add.overlap(robot, baterie, collectBaterie, null, this);
-
+  // Création des caisses
   caisses = this.physics.add.group({
     key: "caisse",
-    repeat: 100,
+    repeat: 5,
     setXY: { x: 100, y: 100, stepX: 150 },
   });
-  // Ajoute des collisions entre les caisses
-  this.physics.add.collider(caisses, caisses);
 
-      // Empêche les caisses de sortir du fond
-      caisses.children.iterate(function(caisse) {
-        caisse.setCollideWorldBounds(true);
-    });
+  // Création de l'objet batterie
+  batterie = this.physics.add.image(400, 300, "batterie");
 
-  // Vérifie les collisions entre le robot et les caisses
+  // Création de la fusée  garfi
+  garfi = this.physics.add.image(400, 300, "garfi");
+
+  // Création des astéroides
+  asteroides = this.physics.add.group({
+    key: "asteroide",
+    repeat: 5,
+    setXY: { x: 400, y: 400, stepX: 150 },
+  });
+
+  robot.setCollideWorldBounds(true);
+  robot.setDepth(1);
+
+  // Caméra
+  this.cameras.main.startFollow(robot);
+  this.cameras.main.setBounds(0, 0, background.width, background.height);
+
+  // Collisions
+  this.physics.add.overlap(robot, batterie, collectBatterie, null, this);
   this.physics.add.collider(robot, caisses, pushCaisse, null, this);
+  this.physics.add.collider(caisses, caisses, null, null, this);
+  //this.physics.add.collider(robot, asteroides, null, null, this);
+  this.physics.add.collider(asteroides, asteroides, null, null, this);
 
-  
+  // Empêche les caisses de sortir du fond
+  caisses.children.iterate(function (caisse) {
+    caisse.setCollideWorldBounds(true);
+  });
 
   // Crée les entrées de clavier pour Q, Z, S, D
   cursors = this.input.keyboard.addKeys({
@@ -87,64 +99,95 @@ function create() {
     right: Phaser.Input.Keyboard.KeyCodes.D,
   });
 
-   // Crée les écouteurs d'événements pour les touches 'g' et 'h'
-   gh = this.input.keyboard.addKeys('G,H');
-
-
-  // Crée garfi
-  garfi = this.physics.add.image(400, 300, 'garfi');
-
   // Définit la vitesse maximale de garfi
-  garfi.setMaxVelocity(100);
+  this.sensors = [];
+  this.sensorGraphics = [];
+
+  for (let i = 0; i < 3; i++) {
+    // Crée 4 capteurs
+
+    let sensorGraphic = this.add.graphics();
+    sensorGraphic.setDepth(0);
+    sensorGraphic.lineStyle(1, 0x00ff00);
+    this.sensorGraphics.push(sensorGraphic);
+  }
 }
 
 function update() {
   // Fait bouger le robot avec les touches Q, Z, S, D
-  if (cursors.left.isDown) {
-    robot.setVelocityX(-500);
-  } else if (cursors.right.isDown) {
-    robot.setVelocityX(500);
-  } else {
-    robot.setVelocityX(0);
+  robot.setVelocity(0);
+  if (cursors.left.isDown) robot.setVelocityX(-500);
+  if (cursors.right.isDown) robot.setVelocityX(500);
+  if (cursors.up.isDown) robot.setVelocityY(-500);
+  if (cursors.down.isDown) robot.setVelocityY(500);
+
+  angle = Phaser.Math.DegToRad(startAngle); // Convertit l'angle de départ en radians
+
+  for (let i = 0; i < this.sensorGraphics.length; i++) {
+    let sensorGraphic = this.sensorGraphics[i];
+    sensorGraphic.clear();
+    sensorGraphic.lineStyle(2, 0x00ff00);
+    sensorGraphic.beginPath();
+    sensorGraphic.moveTo(robot.x, robot.y);
+    sensorGraphic.lineTo(
+      robot.x + Math.cos(angle) * 100,
+      robot.y + Math.sin(angle) * 100
+    );
+    sensorGraphic.strokePath();
+
+    // Crée un rayon à partir de la position du robot jusqu'à la fin du capteur
+    let ray = new Phaser.Geom.Line(
+      robot.x,
+      robot.y,
+      robot.x + Math.cos(angle) * 100,
+      robot.y + Math.sin(angle) * 100
+    );
+
+    // Si une caisse est détectée
+    caisses.children.iterate(function (caisse) {
+      if (Phaser.Geom.Intersects.LineToRectangle(ray, caisse.getBounds())) {
+        console.log("Caisse détectée !");
+        // IMPORTANTTTTTTTTTTTTTTTTTTTTTTTTT !!!!!!!!!!!!!!
+        robot.setVelocity(
+          robot.body.velocity.x / 2,
+          -robot.body.velocity.y / 2
+        );
+
+        // Vous pouvez ajouter du code ici pour réagir à la détection de la caisse
+      }
+    });
+
+    // Si un astéroide est détecté
+    asteroides.children.iterate(function (asteroide) {
+      if (Phaser.Geom.Intersects.LineToRectangle(ray, asteroide.getBounds())) {
+        console.log("Asteroide détecté !");
+        let position = determinePosition(robot, asteroide);
+        console.log(position);
+        if (position === "up" && cursors.up.isDown)
+          robot.setVelocity(robot.body.velocity.x, 0);
+        if (position === "down" && cursors.down.isDown)
+          robot.setVelocity(robot.body.velocity.x, 0);
+        if (position === "left" && cursors.left.isDown)
+          robot.setVelocity(0, robot.body.velocity.y);
+        if (position === "right" && cursors.right .isDown)
+          robot.setVelocity(0, robot.body.velocity.y);
+      }
+    });
+
+    angle += Phaser.Math.DegToRad(angleIncrement); // Augmente l'angle pour le prochain capteur
   }
 
-  if (cursors.up.isDown) {
-    robot.setVelocityY(-500);
-  } else if (cursors.down.isDown) {
-    robot.setVelocityY(500);
-  } else {
-    robot.setVelocityY(0);
-  }
-
-  // Déplace les caisses poussées par le robot
-  caisses.children.iterate(function (caisse) {
-    if (caisse.body.touching.left && cursors.right.isDown) {
-      caisse.setVelocityX(200);
-    } else if (caisse.body.touching.right && cursors.left.isDown) {
-      caisse.setVelocityX(-200);
-    } else if (caisse.body.touching.up && cursors.down.isDown) {
-      caisse.setVelocityY(200);
-    } else if (caisse.body.touching.down && cursors.up.isDown) {
-      caisse.setVelocityY(-200);
-    } else {
-      caisse.setVelocity(0);
-    }
-  });
-  // Modifie la vitesse de garfi lorsque 'g' ou 'h' est enfoncé
-  if (gh.G.isDown) {
-    garfiSpeed -= 10;
-} else if (gh.H.isDown) {
-    garfiSpeed += 10;
+  // Fait en sorte que garfi se déplace vers le robot
+  this.physics.velocityFromAngle(
+    Phaser.Math.Angle.Between(garfi.x, garfi.y, robot.x, robot.y),
+    garfiShpeed,
+    garfi.body.velocity
+  );
 }
 
-// Fait en sorte que garfi se déplace vers le robot
-this.physics.velocityFromAngle(Phaser.Math.Angle.Between(garfi.x, garfi.y, robot.x, robot.y), garfiSpeed, garfi.body.velocity);
-
-}
-
-function collectBaterie(robot, baterie) {
-  // Fait disparaître la baterie
-  baterie.disableBody(true, true);
+function collectBatterie(robot, batterie) {
+  // Fait disparaître la batterie
+  batterie.disableBody(true, true);
 
   // Termine la partie
   // Vous pouvez remplacer cette ligne par le code pour terminer la partie
@@ -152,27 +195,27 @@ function collectBaterie(robot, baterie) {
 }
 
 function pushCaisse(robot, caisse) {
-    // Cette fonction est appelée lorsque le robot entre en collision avec une caisse
-    // Vous pouvez ajouter du code ici si nécessaire
+  // Cette fonction est appelée lorsque le robot entre en collision avec une caisse
 
-    // Détermine la direction du robot
-    let robotDirection = new Phaser.Math.Vector2();
-    if (cursors.left.isDown) {
-        robotDirection.x = -1;
-    } else if (cursors.right.isDown) {
-        robotDirection.x = 1;
-    }
+  // Détermine la direction du robot
+  let robotDirection = new Phaser.Math.Vector2();
+  if (cursors.left.isDown) robotDirection.x = -1;
+  if (cursors.right.isDown) robotDirection.x = 1;
+  if (cursors.up.isDown) robotDirection.y = -1;
+  if (cursors.down.isDown) robotDirection.y = 1;
 
-    if (cursors.up.isDown) {
-        robotDirection.y = -1;
-    } else if (cursors.down.isDown) {
-        robotDirection.y = 1;
-    }
+  caisse.setVelocity(robotDirection.x * 10, robotDirection.y * 10);
+  caisse.setVelocity(0, 0);
+}
 
-    // Déplace la caisse dans la direction du robot
-    caisse.body.velocity.x = robotDirection.x * 200;
-    caisse.body.velocity.y = robotDirection.y * 200;
-
-
-    
+function determinePosition(robot, entity) {
+  let x = robot.x - entity.x;
+  let y = robot.y - entity.y;
+  let distance = Math.sqrt(x * x + y * y);
+  //return up/down/left/right depending on the position of the entity
+  if (Math.abs(x) > Math.abs(y)) {
+    return x > 0 ? "left" : "right";
+  } else {
+    return y > 0 ? "up" : "down";
+  }
 }
