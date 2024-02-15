ec3d2498788bc0f972d8dae7fbf171f29a57d157
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2024-02-15 14:45:56 +0100

    fix PartiesServices.js

--- api.nono/services.nono/src/services/PartiesServices.js
+++ api.nono/services.nono/src/services/PartiesServices.js
@@ -3,27 +3,22 @@ import knexConfig from '../configs/db.config.js'
 
 const db = knex(knexConfig);
 
-class PartiesServices {
-    async updatePartyStatus(partyId, nouvelEtat){
-        await db('parties').where('id', '=', partyId).update({status: nouvelEtat});
-    }
-
-    async getParty(id) {
-         await db('parties').select('*').where('id', '=', id).first();
-    }
-
-    async createParty(envrionnement_id, user_email){
-        let token = crypto.randomUUID().toString();
-        const insertedPartie = await db('parties').insert({
-            user_email: user_email,
-            environnement_id: environnement_id,
-            status: "CREATED",
-            token: token
-        });
-
-        return await db('parties').where('id', insertedPartie[0]).first();
-    }
-
+export const getParty = async (token) => {
+    await db('parties').select('*').where('token', '=', token).first();
 }
 
-export default PartiesServices;
+export const updatePartyStatus = async (token, nouvelEtat) => {
+    await db('parties').where('token', '=', token).update({status: nouvelEtat});
+}
+
+export const createParty = async (environnement_id, user_email) => {
+    let token = crypto.randomUUID().toString();
+    const insertedPartie = await db('parties').insert({
+        user_email: user_email,
+        environnement_id: environnement_id,
+        status: "CREATED",
+        token: token
+    });
+
+    return db('parties').where('id', insertedPartie[0]).first();
+}
