a98853a78d9df64c0b8316c5fdc6e834d589a8a7
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2024-02-15 12:49:45 +0100

    add getEnvironnementsAction.js

--- api.nono/services.nono/src/services/EnvironnementsServices.js
+++ api.nono/services.nono/src/services/EnvironnementsServices.js
@@ -1,114 +1,108 @@
 import axios from "axios";
-import knex from "knex";
-import knexConfig from '../configs/db.config.js'
 
 const directusURL = 'http://directus:8055/graphql';
 
-class EnvironnementsServices {
-    async getEnvironnements() {
-        const query = `
-        query Environnements {
-            Environnement {
-                id
-                nom
-                robot_id {
-                    Robot_id {
-                        id
-                        nom
-                        type
-                        batterie
-                        posX
-                        posY
-                        createDate
-                    }
-                }
-                obstacle_id {
-                    Obstacles_id {
-                        id
-                        obstacleType
-                        posX
-                        posY
-                    }
-                }
-                Configuration_id {
+export const getEnvironnements = async () => {
+    const query = `
+    query Environnements {
+        Environnement {
+            id
+            nom
+            robot_id {
+                Robot_id {
                     id
-                    size
-                    densiteObst
+                    nom
+                    type
+                    batterie
+                    posX
+                    posY
+                    createDate
                 }
             }
-        }
-    `;
-
-        try {
-            const response = await axios({
-                method: "post",
-                url: directusURL,
-                data: {
-                    query: query
-                },
-                headers: {
-                    'Content-Type': 'application/json',
+            obstacle_id {
+                Obstacles_id {
+                    id
+                    obstacleType
+                    posX
+                    posY
                 }
-            });
-
-            return response.data;
-        } catch (error) {
-            console.error("Erreur Axios :", error.response ? error.response.data : error.message);
-            throw error;
+            }
+            Configuration_id {
+                id
+                size
+                densiteObst
+            }
         }
     }
+`;
 
-    async getEnvironnementById(id){
-        const query = `
-        query Environnements_by_id {
-            Environnement_by_id(id: ${id}) {
-                id
-                nom
-                robot_id {
-                    Robot_id {
-                        id
-                        nom
-                        type
-                        batterie
-                        posX
-                        posY
-                        createDate
-                    }
-                }
-                obstacle_id {
-                    Obstacles_id {
-                        id
-                        obstacleType
-                        posX
-                        posY
-                    }
-                }
-                Configuration_id {
-                    id
-                    size
-                    densiteObst
-                }
+    try {
+        const response = await axios({
+            method: "post",
+            url: directusURL,
+            data: {
+                query: query
+            },
+            headers: {
+                'Content-Type': 'application/json',
             }
-        }
-    `;
+        });
 
-        try {
-            const response = await axios({
-                method: "post",
-                url: directusURL,
-                data: {
-                    query: query
-                },
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            });
-            return response.data;
-        } catch (error) {
-            console.error("Erreur Axios :", error.response ? error.response.data : error.message);
-            throw error;
-        }
+        return response.data;
+    } catch (error) {
+        console.error("Erreur Axios :", error.response ? error.response.data : error.message);
+        throw error;
     }
 }
 
-export default EnvironnementsServices;
+const getEnvironnementById = async (id) => {
+    const query = `
+    query Environnements_by_id {
+        Environnement_by_id(id: ${id}) {
+            id
+            nom
+            robot_id {
+                Robot_id {
+                    id
+                    nom
+                    type
+                    batterie
+                    posX
+                    posY
+                    createDate
+                }
+            }
+            obstacle_id {
+                Obstacles_id {
+                    id
+                    obstacleType
+                    posX
+                    posY
+                }
+            }
+            Configuration_id {
+                id
+                size
+                densiteObst
+            }
+        }
+    }
+`;
+
+    try {
+        const response = await axios({
+            method: "post",
+            url: directusURL,
+            data: {
+                query: query
+            },
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Erreur Axios :", error.response ? error.response.data : error.message);
+        throw error;
+    }
+}
