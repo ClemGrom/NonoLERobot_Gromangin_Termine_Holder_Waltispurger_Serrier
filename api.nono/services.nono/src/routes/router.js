3c264def8a433aeb3fb33454b9448a913a665b03
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2024-02-15 12:53:26 +0100

    amj docker compose + add route getEnv

--- api.nono/services.nono/src/routes/router.js
+++ api.nono/services.nono/src/routes/router.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import getEnvironnementsAction from "../actions/getEnvironnementsAction.js";
 
 const router = express.Router();
 
 router
     .route("/environnements")
+    .get(getEnvironnementsAction)
     .all((req, res, next) => next(405));
 
 router
