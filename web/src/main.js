1b727e5500129dfb8464d0bebd5185072bebf361
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2024-02-19 23:39:23 +0100

    add plugins

--- web/src/main.js
+++ web/src/main.js
@@ -3,8 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import toast from './plugins/toast'
+import 'vue3-toastify/dist/index.css'
 
 const app = createApp(App)
+
+app.use(toast)
 
 app.use(router)
 
