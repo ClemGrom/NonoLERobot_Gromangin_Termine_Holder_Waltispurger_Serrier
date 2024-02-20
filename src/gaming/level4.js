c36fe2a6f16f3f685557d1bee7295a5a233a2aff
Jules <jules.holder.54@gmail.com>
2024-02-20 16:45:10 +0100

    refactor: level4.js avec prevision de collisions et rotations selon la direction prise, manque rotation des capteurs

--- src/gaming/level4.js
+++ src/gaming/level4.js
@@ -14,12 +14,10 @@ const config = {
         update: update,
     },
 };
-
-let game = new Phaser.Game(config);
+new Phaser.Game(config);
 let robot;
 let cursors;
 let background;
-let garfi;
 let gh;
 let angle;
 let startAngle = -30; // Angle de départ
@@ -98,6 +96,7 @@ function create() {
 }
 
 function update() {
+    //Initie le déplacement du robot à chaque frame
     robot.setVelocity(vx, vy);
 
     // Fait bouger le robot avec les touches Q, Z, S, D
@@ -108,6 +107,7 @@ function update() {
 
     angle = Phaser.Math.DegToRad(startAngle); // Convertit l'angle de départ en radians
 
+    // Dessine les capteurs du robot et vérifie les collisions
     for (let i = 0; i < this.sensorGraphics.length; i++) {
         let sensorGraphic = this.sensorGraphics[i];
         sensorGraphic.clear();
@@ -121,7 +121,7 @@ function update() {
         sensorGraphic.strokePath();
 
         // Crée un rayon à partir de la position du robot jusqu'à la fin du capteur
-        let ray = new Phaser.Geom.Line(
+        new Phaser.Geom.Line(
             robot.x,
             robot.y,
             robot.x + Math.cos(angle) * 100,
@@ -129,41 +129,53 @@ function update() {
         );
 
         asteroidesHaut.children.iterate(function (asteroide) {
-            if (Phaser.Geom.Intersects.LineToRectangle(ray, asteroide.getBounds())) {
-                //console.log("Asteroide du haut détecté !");
-                let teta = determineAngleFromOrigin(robot);
-                let l = robot.width;
-                let r = (l / 2) * ((this.Vl + this.Vr) / (this.Vr - this.Vl));
-                let omega = (this.Vr - this.Vl) / l;
-                let ICC2 = {x: asteroide.x, y: asteroide.y};
-                let matrice1 = math.matrix([[Math.cos(omega), -Math.sin(omega), 0], [Math.sin(omega), Math.cos(omega), 0], [0, 0, 1]]);
-                let matrice2 = math.matrix([robot.x - ICC2.x, robot.y - ICC2.y, teta]);
-                let matrice3 = math.matrix([ICC2.x, ICC2.y, omega]);
-                let result = math.add(math.multiply(matrice1, matrice2), matrice3);
-                let position = determinePosition(robot, asteroide);
-                moveTo(result, position, i);
-                ray.angle = robot.angle;
-            }
+            let data = determineFuturePoint(robot, asteroide, 1, null)
+            if ((data != null) && i === data.infos.rayon) moveTo(data, i)
         });
 
         asteroidesBas.children.iterate(function (asteroide) {
-            if (Phaser.Geom.Intersects.LineToRectangle(ray, asteroide.getBounds())) {
-                //console.log("Asteroide du bas détecté !");
-                let teta = determineAngleFromOrigin(robot);
-                let l = robot.width;
-                let r = (l / 2) * ((this.Vl + this.Vr) / (this.Vr - this.Vl));
-                let omega = (this.Vr - this.Vl) / l;
-                let ICC2 = {x: asteroide.x, y: asteroide.y};
-                let matrice1 = math.matrix([[Math.cos(omega), -Math.sin(omega), 0], [Math.sin(omega), Math.cos(omega), 0], [0, 0, 1]]);
-                let matrice2 = math.matrix([robot.x - ICC2.x, robot.y - ICC2.y, teta]);
-                let matrice3 = math.matrix([ICC2.x, ICC2.y, omega]);
-                let result = math.add(math.multiply(matrice1, matrice2), matrice3);
-                let position = determinePosition(robot, asteroide);
-                moveTo(result, position, i);
-            }
+            let data = determineFuturePoint(robot, asteroide, 1, null);
+            if ((data != null) && i === data.infos.rayon) moveTo(data, i)
         });
 
         angle += Phaser.Math.DegToRad(angleIncrement); // Augmente l'angle pour le prochain capteur
+    }
+}
+
+function determineFuturePoint(robot, asteroide, index, infos) {
+    if (infos != null) {
+        return {index: index, infos: infos};
+    }
+    if (index > 3) {
+        return null;
+    } else {
+        let teta = determineAngleFromOrigin(robot);
+        let l = robot.width;
+        let omega = (this.Vr - this.Vl) / l;
+        let ICC2 = {x: asteroide.x, y: asteroide.y};
+        let matrice1 = math.matrix([[Math.cos(omega), -Math.sin(omega), 0], [Math.sin(omega), Math.cos(omega), 0], [0, 0, 1]]);
+        let matrice2 = math.matrix([robot.x - ICC2.x, robot.y - ICC2.y, teta]);
+        let matrice3 = math.matrix([ICC2.x, ICC2.y, omega]);
+        let result = math.add(math.multiply(matrice1, matrice2), matrice3);
+        let futureRobot = {x: result.get([0]), y: result.get([1])};
+        for (let j = 0; j < 3; j++) {
+            let Iangle = Phaser.Math.DegToRad(startAngle + 30 * j);
+            let ray = new Phaser.Geom.Line(
+                futureRobot.x,
+                futureRobot.y,
+                futureRobot.x + Math.cos(Iangle) * 100,
+                futureRobot.y + Math.sin(Iangle) * 100
+            );
+            if (Phaser.Geom.Intersects.LineToRectangle(ray, asteroide.getBounds())) {
+                return determineFuturePoint(futureRobot, asteroide, index, {
+                    x: futureRobot.x,
+                    y: futureRobot.y,
+                    rayon: j
+                })
+            }
+        }
+        return determineFuturePoint(futureRobot, asteroide, index + 1, null);
+
     }
 }
 
@@ -176,15 +188,14 @@ function update() {
 function determinePosition(robot, entity) {
     let x = robot.x - entity.x;
     let y = robot.y - entity.y;
-    let distance = Math.sqrt(x * x + y * y);
-    let s = x > 0 ? "left " : "right ";
-    return s += y > 0 ? "up " : "down";
+    Math.sqrt(x * x + y * y);
+    return x > 0 ? "left " : "right " + y > 0 ? "up " : "down";
 }
 
 /**
  * Determine l'angle entre le robot et l'origine
- * @param robot
- * @returns {number}
+ * @param robot :Object les données du robot
+ * @returns number
  */
 function determineAngleFromOrigin(robot) {
     let x = robot.x;
@@ -194,28 +205,26 @@ function determineAngleFromOrigin(robot) {
 
 /**
  * Oriente et déplace le robot vers la position donnée
- * @param result corerspond aux coordonnées du point vers lequel se déplacer et l'angle à prendre
+ * @param data :Object  les données de la position future du robot
+ * @param i : int l'index du capteur
  */
-function moveTo(result, position, i) {
-    let x = result.get([0]);
-    let y = result.get([1]);
-    if (i == 0) {
-        vx = Math.cos(angle) * Vl;
-        vy = -Math.sin(angle) * Vr;
-        console.log("Capteur haut déclenché");
-        robot.angle += result.get([2]);
+function moveTo(data, i) {
+    if (i === 0) {
+        let Nangle = Phaser.Math.DegToRad(-15 * (data.index + 1));
+        vx = Math.cos(Nangle) * Vl;
+        vy = -Math.sin(Nangle) * Vr;
+        robot.angle = Phaser.Math.RadToDeg(-Nangle);
     }
-    if (i == 1) {
-        vx = -Math.cos(angle) * Vl;
-        vy = -Math.sin(angle) * Vr;
-        console.log("Capteur devant déclenché");
-        robot.angle += result.get([2]);
+    if (i === 1) {
+        let Nangle = Phaser.Math.DegToRad(180);
+        vx = -Math.cos(Nangle) * Vl;
+        vy = -Math.sin(Nangle) * Vr;
+        robot.angle = Phaser.Math.RadToDeg(-Nangle);
     }
-    if (i == 2) {
-        vx = Math.cos(angle) * Vl;
-        vy = -Math.sin(angle) * Vr;
-        console.log("Capteur bas déclenché");
-        robot.angle += -result.get([2]);
+    if (i === 2) {
+        let Nangle = Phaser.Math.DegToRad(15 * (data.index + 1));
+        vx = Math.cos(Nangle) * Vl;
+        vy = -Math.sin(Nangle) * Vr;
+        robot.angle = Phaser.Math.RadToDeg(-Nangle);
     }
-
 }
