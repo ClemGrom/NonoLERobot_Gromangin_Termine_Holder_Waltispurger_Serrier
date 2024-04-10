dc2d0fa0f32b8cdb8ec81e40b261c9c5677f796c
ClemGrom <clement.gromangin5@etu.univ-lorraine.fr>
2024-04-10 15:09:05 +0200

    aboutpage

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container mx-auto p-6 bg-white rounded shadow-lg">
+    <h1 class="text-4xl font-bold mb-2">Membres du projet :</h1>
+    <ul class="space-y-4">
+      <li class="flex items-center space-x-4">
+        <img src="@/assets/images/clem.jpg" alt="Gromangin Clément" class="w-24 h-24 rounded-full shadow" />
+        <h2 class="text-2xl font-semibold">Gromangin Clément</h2>
+      </li>
+      <!-- ... -->
+    </ul>
   </div>
 </template>
-
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>
