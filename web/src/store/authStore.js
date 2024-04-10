3e84d114e2a93497b950f4aaccfb934e3e255a17
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2024-04-10 12:54:45 +0200

    maj authstore

--- web/src/store/authStore.js
+++ web/src/store/authStore.js
@@ -5,28 +5,22 @@ export const useAuthStore = defineStore('auth', {
         return {
             isConnected: false,
             user_email: "",
-            accessToken: "",
-            expireIn: ""
         }
     },
     actions: {
-        setconnected(email, token, expire) {
+        setconnected(email) {
             this.isConnected = true;
             this.user_email = email;
-            this.accessToken = token;
-            this.expireIn = expire;
         },
         disconnect() {
             this.isConnected = false;
             this.user_email = "";
-            this.accessToken = "";
-            this.expireIn = "";
         },
     },
     persist: {
         enabled: true,
         strategies: [
-            {storage: localStorage, paths:['user_email', 'accessToken', 'expireIn']}
+            {storage: localStorage, paths:['user_email']}
         ]
     }
 })
