aa6251df5ed28caefeb7d214bbd128fe43ecc30c
ClemGrom <clement.gromangin5@etu.univ-lorraine.fr>
2024-04-10 16:30:02 +0200

    aboutpage

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -3,10 +3,26 @@
     <h1 class="text-4xl font-bold mb-2">Membres du projet :</h1>
     <ul class="space-y-4">
       <li class="flex items-center space-x-4">
-        <img src="@/assets/images/clem.jpg" alt="Gromangin Clément" class="w-24 h-24 rounded-full shadow" />
+        <img src="@/assets/images/clem.jpg" alt="Moi" class="w-24 h-24 rounded-full shadow" />
         <h2 class="text-2xl font-semibold">Gromangin Clément</h2>
       </li>
-      <!-- ... -->
+      <li class="flex items-center space-x-4">
+        <img src="@/assets/images/jules.jpg" alt="Lui" class="w-24 h-24 rounded-full shadow" />
+        <h2 class="text-2xl font-semibold">Holder Jules</h2>
+      </li>
+      <li class="flex items-center space-x-4">
+        <img src="@/assets/images/julie.png" alt="eux" class="w-24 h-24 rounded-full shadow" />
+        <h2 class="text-2xl font-semibold">Waltispurger-Despré Julie</h2>
+      </li>
+      <li class="flex items-center space-x-4">
+        <img src="@/assets/images/termine.jpg" alt="il" class="w-24 h-24 rounded-full shadow" />
+        <h2 class="text-2xl font-semibold">Termine Matthéo</h2>
+      </li>
+      <li class="flex items-center space-x-4">
+        <img src="@/assets/images/serrier.jpg" alt="ça" class="w-24 h-24 rounded-full shadow" />
+        <h2 class="text-2xl font-semibold">Serrier Mathéo</h2>
+      </li>
+    
     </ul>
   </div>
 </template>
