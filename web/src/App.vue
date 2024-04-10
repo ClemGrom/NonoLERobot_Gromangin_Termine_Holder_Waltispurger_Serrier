94af8d2bfd7fcd43b4c098cdff69da847c44bbc7
Jules <jules.holder.54@gmail.com>
2024-04-10 10:16:52 +0200

    refatcor: changement intitulés

--- web/src/App.vue
+++ web/src/App.vue
@@ -75,13 +75,13 @@ export default {
         <div
             class=" max-sm:text-xs max-sm:mr-1.5 sm:text-base text-white text-2xl font-bold py-2 px-4 rounded-xl bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 mr-3 hover:transition duration-300 ease-in-out transform hover:scale-105">
           <RouterLink to="/robotMoteur">
-            <button class="h-full w-full">Jouer</button>
+            <button class="h-full w-full">Lancer la simulation</button>
           </RouterLink>
         </div>
         <div
             class=" max-sm:text-xs max-sm:mr-1.5 sm:text-base text-white text-2xl font-bold py-2 px-4 rounded-xl bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 mr-3 hover:transition duration-300 ease-in-out transform hover:scale-105">
           <RouterLink to="/previous-levels">
-            <button class="h-full w-full">Jouer aux anciens niveaux</button>
+            <button class="h-full w-full">Anciens niveaux</button>
           </RouterLink>
         </div>
 
