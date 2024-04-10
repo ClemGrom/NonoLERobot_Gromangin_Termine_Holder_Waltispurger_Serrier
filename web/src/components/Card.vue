d937812be94f3922b370c6feba65a04b1e0b6c30
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2024-04-10 13:38:44 +0200

    début style page accueil

---
+++ web/src/components/Card.vue
@@ -0,0 +1,75 @@
+<!-- MyComponent.vue -->
+<template>
+  <div :class="cardClasses" class="card justify-center items-center block m-2">
+    <div class="title">{{ title }}</div>
+    <div class="description">{{ description }}</div>
+    <router-link class="link" :to="routerLink">{{ routerText }}</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    routerLink: {
+      type: String,
+      required: true
+    },
+    routerText: {
+      type: String,
+      default: 'En savoir plus'
+    },
+    backgroundColor: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    cardClasses() {
+      const classes = [
+        'card',
+        'text-white',
+        'text-2xl',
+        'font-bold',
+        'py-2',
+        'px-4',
+        'rounded-xl',
+        'hover:bg-gradient-to-br',
+        'focus:ring-4',
+        'focus:outline-none',
+        'focus:ring-green-300',
+        'dark:focus:ring-red-800',
+        'mr-3',
+        'hover:transition',
+        'duration-300',
+        'ease-in-out',
+        'transform',
+        'hover:scale-105',
+        'mb-14'
+      ];
+
+      if (this.backgroundColor) {
+        classes.push(this.backgroundColor);
+      }
+
+      return classes.join(' ');
+    }
+  }
+};
+</script>
+
+<style>
+.card {
+  height: 80%;
+  align-content: center;
+  align-items: center;
+  text-align: justify;
+}
+</style>
